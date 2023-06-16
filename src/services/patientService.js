306a9273ce3d2a410702b7745ac5c678e418ff6c
nguyenvuthien <thien180102@gmail.com>
2023-06-16 09:12:54 +0700

    nodej

---
+++ src/services/patientService.js
@@ -0,0 +1,62 @@
+import db from "../models/index";
+require('dotenv').config();
+import emailService from '../services/emailService'
+
+let postBookAppointment = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.email || !data.doctorId || !data.timeType || !data.date
+                || !data.fullName
+            ) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing parameter'
+                })
+            } else {
+
+                await emailService.sendSimpleEmail({
+                    receiverEmail: data.email,
+                    patientName: data.fullName,
+                    time: data.timeString,
+                    doctorName: data.doctorName,
+                    language: data.language,
+                    redirectLink: 'https://www.facebook.com/'
+
+                })
+                //upsert patient
+                let user = await db.User.findOrCreate({
+                    where: { email: data.email },
+                    defaults: {
+                        email: data.email,
+                        roleId: 'R3'
+                    },
+                });
+
+                //create a booking record
+                if (user && user[0]) {
+                    await db.Booking.findOrCreate({
+                        where: { patientId: user[0].id },
+                        defaults: {
+                            statusId: 'S1',
+                            doctorId: data.doctorId,
+                            patientId: user[0].id,
+                            date: data.date,
+                            timeType: data.timeType
+                        }
+                    })
+                }
+
+                resolve({
+                    errCode: 0,
+                    errMessage: 'Save infor patient succeed!'
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+module.exports = {
+    postBookAppointment: postBookAppointment
+}
